dd0754068dafcd2fd0f1791c77f0138c115cb625
charliemagati <charliemagati@gmail.com>
2022-03-27 07:36:11 -0700

    adding tab idents on lines

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main(void)
 /**
-*Main Entry point
+*Main - Entry point
 *
-*Return zero (0)
-*
+*Return: Always 0 (Success)
 */
+
+int main(void)
+  
 {
 	   puts("\"Programming is like building a multilingual puzzle");
 	   return (0);
